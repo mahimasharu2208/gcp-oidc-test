7d9009f30508d7780d32d3c2d29bfe89093175c4
Mahima <mahima.sharu@hashicorp.com>
2025-09-09 12:02:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,1 @@
-variable "project_id" {
-  description = "The GCP project ID"
-  type        = string
-}
 
-variable "region" {
-  description = "The region to deploy resources in"
-  type        = string
-  default     = "us-central1"
-}
